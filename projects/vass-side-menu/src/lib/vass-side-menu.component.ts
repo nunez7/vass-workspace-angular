6f77159569e25406c6130eb5072ffd0caeb8213a
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-23 14:42:16 -0700

    Funcionalidad y estilo de la libreria

--- projects/vass-side-menu/src/lib/vass-side-menu.component.ts
+++ projects/vass-side-menu/src/lib/vass-side-menu.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+export enum TitleColor {
+  red = 'text-red-500',
+  green = 'text-green-500',
+  blue = 'text-blue-500',
+  purple = 'text-purple-500',
+}
 
 @Component({
   selector: 'lib-vass-side-menu',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      vass-side-menu works!
-    </p>
-  `,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './vass-side-menu.component.html',
   styles: ``
 })
 export class VassSideMenuComponent {
+
+  isAuthenticated = input(false);
+  titleColor = input<TitleColor>(TitleColor.blue);
+
+  signOut = output();
+  signIn = output();
+
+
 
 }
--- projects/vass-testbed-app/src/app/app.component.ts
+++ projects/vass-testbed-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { VassSideMenuComponent } from 'vass-side-menu';
+import { VassSideMenuComponent, TitleColor } from 'vass-side-menu';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,7 @@ import { VassSideMenuComponent } from 'vass-side-menu';
 })
 export class AppComponent {
   title = 'vass-testbed-app';
+
+  TitleColor = TitleColor;
+  isAuthenticated = signal(true);
 }
