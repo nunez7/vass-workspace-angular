d2236e692d15d43ab18e0e74edce01b2f59c27d1
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-23 13:44:38 -0700

    Importando librerias de un proyecto a otro

--- projects/vass-testbed-app/src/app/app.component.ts
+++ projects/vass-testbed-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VassSideMenuComponent } from 'vass-side-menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VassSideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
